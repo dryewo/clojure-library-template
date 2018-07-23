b7fe955061a87dee44f588fe6ad8e53eb9b790bf
Dmitrii Balakhonskii <dmitrii.balakhonskii@zalando.de>
2018-07-24 00:12:33 +0200

    Version 0.2.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject dryewo-lib/lein-template "0.1.0"
+(defproject dryewo-lib/lein-template "0.2.0"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
